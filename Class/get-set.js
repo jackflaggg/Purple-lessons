2b6b998f929fa8c2393cc1d6dd284ef7c67f803d
jackflaggg <rasul.khamzinnn@gmail.com>
2024-06-19 17:43:32 +0500

    add get-set

--- Class/get-set.js
+++ Class/get-set.js
@@ -1,37 +1,51 @@
 'use strict';
 
-const task = {
-	title: 'task1',
-	dueTo: new Date('2023/01/01'),
-
-	get isOverDue() {
-		return this.dueTo.getTime() < new Date().getTime();
-	},
-	set isOverDue(isOver) {
-		if (!isOver) this.dueTo = new Date().getTime();
-	},
-}
-console.log(task.isOverDue);
-
-task.isOverDue = false;
-
-console.log(task);
+// const task = {
+// 	title: 'task1',
+// 	dueTo: new Date('2023/01/01'),
+//
+// 	get isOverDue() {
+// 		return this.dueTo.getTime() < new Date().getTime();
+// 	},
+// 	set isOverDue(isOver) {
+// 		if (!isOver) this.dueTo = new Date().getTime();
+// 	},
+// }
+// console.log(task.isOverDue);
+//
+// task.isOverDue = false;
+//
+// console.log(task);
 
 class Task {
-	constructor(title, dueTo){
+	constructor(title, dueDate){
 		this.title = title;
-		this.dueTo = dueTo;
+		this.dueDate = dueDate;
 	}
 	get isOverdue(){
-		return this.dueTo < new Date().getTime()
+		return this.dueDate < new Date().getTime()
 	}
 	set dueDate(date) {
 		if (date < new Date()) return;
-		this.dueTo = date;
+		this._dueDate = date;
 	}
 }
 
 const taskClass = new Task('task One', new Date())
-console.log(taskClass.dueDate = new Date('2024/01/01'));
+console.log(taskClass.dueDate = new Date('2025/01/01'));
 
-
+// class Example {
+// 	_protectedProperty = 42;
+// 	#privateProperty = 'secret!'
+//
+// 	get getProtectedProperty() {
+// 		return this._protectedProperty;
+// 	}
+//
+// 	get getPrivateField() {
+// 		return this.#privateProperty;
+// 	}
+// }
+//
+// const instance = new Example();
+// console.log(instance)
