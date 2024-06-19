d5ee790bd23953d17e0c8a0eab1d031cfdb47006
jackflaggg <rasul.khamzinnn@gmail.com>
2024-06-19 17:51:57 +0500

    add static

--- Class/static.js
+++ Class/static.js
@@ -1,1 +1,17 @@
 'use strict';
+
+class Human {
+	static a = 1;
+	static sum(a, b){
+		return a + b;
+	}
+	constructor(a, b) {
+		this._a = a;
+		this._b = b;
+	}
+}
+
+console.log(Human.sum(1, 2));
+
+const newHuman = new Human(2, 2);
+console.log(newHuman);
